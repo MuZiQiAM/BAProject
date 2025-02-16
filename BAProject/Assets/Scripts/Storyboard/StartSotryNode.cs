45062f7d0359e3cf80358896e6fac2025d916f89
MuZiQiAM <98825250+MuZiQiAM@users.noreply.github.com>
2025-02-16 18:32:42 +0100

    init commit

---
+++ BAProject/Assets/Scripts/Storyboard/StartSotryNode.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class StartSotryNode : BaseNode {
+
+	[Output] public NodePort exit;
+
+	public override string GetString()
+	{
+		return "Start";
+	}
+	
+	
+}
---
+++ BAProject/Assets/Scripts/XNode/BaseNode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using XNode;
+
+public class BaseNode : Node
+{
+    public virtual string GetString()
+    {
+        return null;
+    }
+
+    public virtual Sprite GetSprite()
+    {
+        return null;
+    }
+
+    public virtual Image GetImage()
+    {
+        return null;
+    }
+}
